8d874f3d1cf7e605a5a28ef99d8baef8840ad164
Sithi5 <ma.sithis@gmail.com>
2021-09-29 17:24:50 +0200

    [feat]: Add normalize function in utils.

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,15 @@
 import io
 import pandas as pd
+from numpy.typing import ArrayLike
 
 from src.global_var import THETA_VALUE_FILE_PATH
 from pandas.core.frame import DataFrame
 
 
 def read_csv_file(csv_file_path) -> DataFrame:
+    """
+    This function read a csv file from the input path and return a DataFrame object.
+    """
     try:
         data = pd.read_csv(csv_file_path)
     except Exception as e:
@@ -15,6 +19,10 @@ def read_csv_file(csv_file_path) -> DataFrame:
 
 
 def get_theta_values_from_file() -> tuple():
+    """
+    This function read the theta values contained in the file and return it.
+    If the file doesn't exist, it will return 0.0 0.0 as default value.
+    """
     try:
         with open(THETA_VALUE_FILE_PATH) as file:
             theta = file.read()
@@ -28,9 +36,17 @@ def write_theta_file(theta_0: float, theta_1: float):
     try:
         with open(THETA_VALUE_FILE_PATH, "w") as file:
             file.write(str(theta_0) + " " + str(theta_1))
-    except io.UnsupportedOperation as e:
-        print("Write theta file fail: ", e)
-        raise
     except Exception as e:
         print("Write theta file fail: ", e)
         raise
+
+
+def maximum_absolute_scaling(data_frame: DataFrame):
+    # Copy the dataframe
+    data_frame_scaled = data_frame.copy()
+    # Apply maximum absolute scaling
+    for column in data_frame_scaled.columns:
+        data_frame_scaled[column] = (
+            data_frame_scaled[column] / data_frame_scaled[column].abs().max()
+        )
+    return data_frame_scaled
